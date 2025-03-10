75758015e751334cf3f771e674b74b5e2f710213
Andrew Forman <97414979+AndrewggForman@users.noreply.github.com>
2025-03-09 23:08:33 -0400

    Adding count the smiley faces challenge

---
+++ CountTheSmileyFaces.js
@@ -0,0 +1,44 @@
+// Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+// Rules for a smiling face:
+
+// Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+// A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+// Every smiling face must have a smiling mouth that should be marked with either ) or D
+// No additional characters are allowed except for those mentioned.
+
+// Valid smiley face examples: :) :D ;-D :~)
+// Invalid smiley faces: ;( :> :} :]
+
+// Example
+// countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+// countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+// countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+// Note
+// In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
+
+
+
+//return the total number of smiling faces in the array
+function countSmileys(arr) {
+    if (arr.length === 0){
+      return 0;
+    }
+    let total = 0;
+    let eyes = [":", ";"];
+    let noses = ["-", "~"];
+    let smiles = [")", "D"];
+    for(const face of arr){
+      if(face.length === 3){
+        if(eyes.includes(face[0]) && noses.includes(face[1]) && smiles.includes(face[2])){
+          total += 1;
+        }
+      } 
+      else if(face.length === 2){
+        if(eyes.includes(face[0]) && smiles.includes(face[1])){
+          total +=1;
+        }
+      }
+    }
+    return total;
+  }
